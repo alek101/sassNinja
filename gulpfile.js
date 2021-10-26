6d68c09af136fd56456877025b6890b745c36d11
alek101 <47416711+alek101@users.noreply.github.com>
2021-10-26 22:34:55 +0200

    purge

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const { src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
     // return src('index.scss')
     return src('shinobi/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('css'))
 }
 
